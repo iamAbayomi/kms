135529d3a8f48cef652f130a5ddd65e8195d8711
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-03-25 23:04:12 +0100

    created routes for users and admin

---
+++ api/routes/admin.routes.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+  const needs = require('../controller/admin.controller')
+  const router = require('express').Router()
+
+  // Create a new Admin User
+  router.post('/', needs.createAdmin)
+
+  // Check if user is admin
+  router.get('/:user_id', needs.checkAdmin)
+
+  // Get the lists of admin
+  router.get('/:note_id', needs.findlistsofAdmins)
+
+//   // Update Notes
+//   router.put('/:note_id', needs.updateNotes)
+
+  app.use('/apis/notes', router)
+}
---
+++ api/routes/notes.routes.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+  const needs = require('../controller/notes.controller')
+  const router = require('express').Router()
+
+  // Create a new Notes for Users
+  router.post('/', needs.createNotes)
+
+  // Retrieve the notes of all Users
+  router.get('/:user_id', needs.findAllNotes)
+
+  // Retrieve one note for one user
+  router.get('/:note_id', needs.findSingleNotes)
+
+  // Update Notes
+  router.put('/:note_id', needs.updateNotes)
+
+  app.use('/apis/notes', router)
+}
