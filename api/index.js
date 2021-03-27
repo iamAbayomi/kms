f6283d4c6d69293bb6b90ae538c555499a729f50
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-03-27 09:50:49 +0100

    added express application

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+// Init body-parser options (inbuilt with express)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Export the server middleware
+module.exports = app
