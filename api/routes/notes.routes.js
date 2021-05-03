be52fa2a86b129fae9d4a4cfcdc6667a475fde46
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-05-03 22:32:52 +0100

    added routes to delete user notes

--- api/routes/notes.routes.js
+++ api/routes/notes.routes.js
@@ -1,18 +1,21 @@
 module.exports = (app) => {
-  const needs = require('../controller/notes.controller')
+  const notes = require('../controller/notes.controller')
   const router = require('express').Router()
 
   // Create a new Notes for Users
-  router.post('/', needs.createNotes)
+  router.post('/', notes.createNotes)
 
   // Retrieve the all notes of one User
-  router.get('/:user_id', needs.findAllNotes)
+  router.get('/:user_id', notes.findAllNotes)
 
   // Retrieve one note for one user
-  router.get('/usernotes/:note_id', needs.findSingleNotes)
+  router.get('/usernotes/:note_id', notes.findSingleNotes)
 
   // Update Notes
-  router.put('/:note_id', needs.updateNotes)
+  router.put('/:note_id', notes.updateNotes)
+
+  // Delete Notes
+  router.delete('/:note_id', notes.deleteNote)
 
   app.use('/apis/notes', router)
 }
