f3937d07db88d21eeb311c496d127ea6e3f15c16
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-05-09 01:44:10 +0100

    changed db to heroku config

--- api/models/index.js
+++ api/models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const config = require('../config/db.config')
+const config = require('../config/heroku.config')
 
 const sequelize = new Sequelize(
   config.DB,
