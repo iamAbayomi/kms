2b38bbe1f30556ab6dbc7faf42b4fe45af228300
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-05-09 02:22:17 +0100

    added heroku database

---
+++ api/config/geroku.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HOST: 'ec2-54-234-28-165.compute-1.amazonaws.com',
+  PORT: '5432',
+  USER: 'vhiaympewkybwh',
+  PASSWORD: '2fb0f83243bf9494d1a9e60cd5a92ad6935ad20facb16d677219e059bead2bb9',
+  DB: 'ddoqnm96q8ea4c',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 3000,
+    idle: 1000
+  }
+}
