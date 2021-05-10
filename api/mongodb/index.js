94ecfef7630a9878c10dc5a1bfff100a36d90af0
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-05-10 09:40:11 +0100

    added Mongoose model

--- api/mongodb/index.js
+++ api/mongodb/index.js
@@ -1,39 +1,49 @@
 /* eslint-disable node/handle-callback-err */
 /* eslint-disable no-console */
-const MongoClient = require('mongodb').MongoClient
-const uri = 'mongodb+srv://root:aLebZrmib8hca8Sl@cluster0.yn8mm.mongodb.net/'
 
-MongoClient.connect(uri, function (err, datab) {
-  if (err) { console.log(err) }
-  const db = datab.db('databaseProject')
-  // console.log('data' + datab)
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://root:aLebZrmib8hca8Sl@cluster0.yn8mm.mongodb.net/databaseProject')
 
-  exports.sendStudents = (req, res) => {
-    db.collection('Students').find().toArray()
-      .then((results) => {
-        res.send(results)
-        console.log(results)
-      })
-      .catch(error => console.error('This is the error' + error))
-  }
+const db = mongoose.connection
 
-  exports.sendCourses = (req, res) => {
-    db.collection('Courses').find().toArray()
-      .then((results) => {
-        res.send(results)
-        console.log(results)
-      })
-      .catch(error => console.error('This is the error' + error))
-  }
-
-  exports.sendResults = (req, res) => {
-    db.collection('Results').find().toArray()
-      .then((results) => {
-        res.send(results)
-        console.log(results)
-      })
-      .catch(error => console.error('This is the error' + error))
-  }
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  // we're connected!
+  console.log('We are connected')
 })
 
-console.log('lo')
+const studentsSchema = new mongoose.Schema({})
+const Student = mongoose.model('Students', studentsSchema, 'Students')
+
+const coursesSchema = new mongoose.Schema({})
+const Courses = mongoose.model('Courses', coursesSchema, 'Courses')
+
+const resultsSchema = new mongoose.Schema({})
+const Results = mongoose.model('Results', resultsSchema, 'Results')
+
+Student.find({})
+  .then((data) => {
+    // res.send(data)
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+Courses.find({})
+  .then((data) => {
+    // res.send(data)
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+Results.find({})
+  .then((data) => {
+    // res.send(data)
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
