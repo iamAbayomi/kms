a0e510645534723ca7e7ed398f068b154b6c1a77
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-06-11 18:32:39 +0100

    removed db synchronization

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // Initializing Database
 const db = require('./models')
-db.sequelize.sync({ alter: true })
+// db.sequelize.sync({ alter: true })
 
 // use cors
 // require('./utilities/cors')(app)
@@ -19,7 +19,7 @@ require('./mongodb/mongo.routes')(app)
 
 // require('./utilities/secretManager')
 
-// console.log('This is the LocalHost' + process.env.LOCAL_DB_USER)
+console.log('This is the LocalHost' + process.env.LOCAL_DB_USER)
 
 require('./routes/user.routes')(app)
 require('./routes/admin.routes')(app)
