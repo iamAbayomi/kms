4f8fa21e492a38a20849ae4dd1e2d68477cb0fd2
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-06-11 16:53:12 +0100

    changed the environmental variables

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: '123456',
-  DB: 'kms_main',
-  dialect: 'mysql',
+  HOST: process.env.LOCAL_DB_HOST,
+  USER: process.env.LOCAL_DB_USER,
+  PASSWORD: process.env.LOCAL_DB_PASSWORD,
+  DB: process.env.LOCAL_DB,
+  dialect: process.env.LOCAL_DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
