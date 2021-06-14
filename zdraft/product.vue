638acae0beaef5a1e15b02e573c6c136f751ebbd
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-06-14 16:16:21 +0100

    added method for product categories

--- zdraft/product.vue
+++ zdraft/product.vue
@@ -3,15 +3,54 @@
     <!-- All user products -->
     <div class="all-products">
       <nuxt-link
-        v-for="item in product_group"
-        :key="item.product_id"
-        class="product-item"
+        class="single-product text-link"
         to="/notes"
       >
         <p>
           Spiinge
         </p>
         <p>22-08-21</p>
+        <div class="horizontal-line" />
+      </nuxt-link>
+      <nuxt-link
+        class="single-product text-link"
+        to="/notes"
+      >
+        <p>
+          Spiinge Webapp
+        </p>
+        <p>22-08-21</p>
+        <div class="horizontal-line" />
+      </nuxt-link>
+      <nuxt-link
+        class="single-product text-link"
+        to="/notes"
+      >
+        <p>
+          Spiinge
+        </p>
+        <p>22-08-21</p>
+        <div class="horizontal-line" />
+      </nuxt-link>
+      <nuxt-link
+        class="single-product text-link"
+        to="/notes"
+      >
+        <p>
+          Fola
+        </p>
+        <p>22-08-21</p>
+        <div class="horizontal-line" />
+      </nuxt-link>
+      <nuxt-link
+        class="single-product text-link"
+        to="/notes"
+      >
+        <p>
+          Confactories
+        </p>
+        <p>22-08-21</p>
+        <div class="horizontal-line" />
       </nuxt-link>
     </div>
     <!-- End of user products declaration -->
@@ -30,5 +69,20 @@ export default {
 </script>
 
 <style >
+
+.all-products{
+  margin: 20px 40px 20px 40px;
+}
+
+.single-product{
+   margin: 20px 40px 20px 40px;
+}
+
+.horizontal-line{
+  margin: 20px 0px;
+  border: rgba(0, 0, 0, 0.145);
+  border-style: solid;
+  border-width: 0px 0px 1px 0px;
+}
 
 </style>
