4c799c6e02ca17c771739eeb8ad494e4fb97ca89
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-06-15 01:03:12 +0100

    created route for product stage

---
+++ api/routes/stage.routes.js
@@ -0,0 +1,21 @@
+module.exports = (app) => {
+  const stages = require('../controller/stage.controller')
+  const router = require('express').Router()
+
+  // Create a new Stages for Users
+  router.post('/', stages.createStage)
+
+  // Retrieve the all stages of one User
+  router.get('/:user_id', stages.findAllStage)
+
+  // Retrieve one stage for one user
+  router.get('/userstages/:stage_id', stages.findSingleStage)
+
+  // Update Stages
+  router.put('/:stage_id', stages.updateStage)
+
+  // Delete Stages
+  router.delete('/:stage_id', stages.deleteStage)
+
+  app.use('/apis/stages', router)
+}
