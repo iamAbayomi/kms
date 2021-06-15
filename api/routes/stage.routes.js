edbfd8d629f8d164ff1e6f2955b70e515af29d37
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-06-15 10:45:13 +0100

    added userstage routes for stage

--- api/routes/stage.routes.js
+++ api/routes/stage.routes.js
@@ -5,11 +5,13 @@ module.exports = (app) => {
   // Create a new Stages for Users
   router.post('/', stages.createStage)
 
+  // Retrieve one stage for one user
+  router.get('/userstage/:stage_id', stages.findSingleStage)
+
   // Retrieve the all stages of one User
   router.get('/:user_id/:product_id', stages.findAllStage)
 
-  // Retrieve one stage for one user
-  router.get('/userstages/:stage_id', stages.findSingleStage)
+  
 
   // Update Stages
   router.put('/:stage_id', stages.updateStage)
