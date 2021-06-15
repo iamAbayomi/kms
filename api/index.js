9de0a720762a9c070b33de7bed65ee81d241b94a
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-06-15 01:02:25 +0100

    changed db name

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // Initializing Database
 const db = require('./models')
-// db.sequelize.sync({ alter: true })
+db.sequelize.sync({ alter: true })
 
 // use cors
 // require('./utilities/cors')(app)
@@ -21,7 +21,8 @@ require('./mongodb/mongo.routes')(app)
 
 require('./routes/user.routes')(app)
 require('./routes/admin.routes')(app)
-require('./routes/notes.routes')(app)
+require('./routes/product.routes')(app)
+require('./routes/stage.routes')(app)
 
 // Export the server middleware
 module.exports = app
