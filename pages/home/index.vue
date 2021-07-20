e36c935939772bb27e5412722dee2d41a81b8644
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-07-20 11:40:58 +0100

    added product-options

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,43 +1,56 @@
 <template>
   <div class="main-section">
     <!-- All user products -->
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
     <div class="all-products">
       <div
         v-for="item in product_group"
         :key="item.product_id"
         class="single-product"
       >
-        <nuxt-link
-          class="text-link"
-          :to="'/productstage/' + item.product_id.toString()"
-        >
-          <div class="product-details">
-            <p>
-              {{ item.product_name }}
-            </p>
-            <p>
-              {{
-                new Date(item.updatedAt)
-                  .toString('YYYY-MM-dd')
-              }}
-            </p>
+        <div class="single-product-design">
+          <nuxt-link
+            class="text-link"
+            :to="'/productstage/' + item.product_id.toString()"
+          >
+            <div class="product-details">
+              <p>
+                {{ item.product_name }}
+              </p>
+              <p>
+                {{
+                  new Date(item.updatedAt)
+                    .toString('YYYY-MM-dd')
+                }}
+              </p>
+            </div>
+          </nuxt-link>
+          <div class="product-options">
+            <div class="text-center">
+              <v-menu offset-y>
+                <template #activator="{ on, attrs }">
+                  <v-btn icon>
+                    <v-icon
+                      dark
+                      class="menu-icon"
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      fas fa-ellipsis-v
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item
+                    v-for="(single, index) in optionsMenu"
+                    :key="index"
+                  >
+                    <v-list-item-title>{{ single.option }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </div>
           </div>
-        </nuxt-link>
-        <div class="product-options">
-          <v-tooltip bottom>
-            <template #activator="{ on, attrs }">
-              <v-btn icon>
-                <v-icon
-                  v-bind="attrs"
-                  v-on="on"
-                  @click="deleteSingleProduct(item.product_id)"
-                >
-                  mdi-trash-can
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Delete</span>
-          </v-tooltip>
         </div>
         <div class="horizontal-line" />
       </div>
@@ -60,6 +73,10 @@ export default {
   middleware: 'auth',
   data () {
     return {
+      optionsMenu: [
+        { option: 'Rename' },
+        { option: 'Delete' }
+      ],
       product_group: [
 
       ],
@@ -130,9 +147,14 @@ export default {
    margin: 20px 40px 0px 40px;
 }
 
+.single-product-design{
+  display: flex;
+  justify-content: space-between;
+}
+
 .product-options{
-    float: right;
-    /* margin: 20px 0px 20px 0px; */
+    /* float: right; */
+    margin: 15px 0px 0px 0px;
     /* position: relative;
     bottom: 20px;
     right: 30px; */
@@ -149,6 +171,15 @@ export default {
   border: rgba(0, 0, 0, 0.145);
   border-style: solid;
   border-width: 0px 0px 1px 0px;
+}
+
+.menu-icon{
+  height: 18px;
+}
+.v-btn--icon.v-size--default .v-icon, .v-btn--fab.v-size--default .v-icon {
+    height: 20px;
+    font-size: 20px;
+    width: 18px;
 }
 
 </style>
